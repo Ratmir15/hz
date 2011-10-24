cf038016da357f1b730fc258f343e0cb1f21d418
Ratmir Panov <rpanov@localhost.(none)>
2011-10-24 15:15:52 +0400

    simplereports

--- mysite/pansionat/views.py
+++ mysite/pansionat/views.py
@@ -1199,6 +1199,59 @@ class OrderForm(ModelForm):
         model = Order
         exclude = ('patient', 'room')
 
+class EnteringForm(forms.Form):
+    report_date = forms.DateField(required=True, label='Дата заезда')
+
+class EnteringReport():
+
+    def process(self, form):
+        cleaned_report_date = form.cleaned_data['report_date']
+        orders = Order.objects.filter(start_date = cleaned_report_date)
+        d = []
+        for order in orders:
+            d.append({'FIO':order.patient.fio(), 'ROOM':order.room.name})
+        z = {'REPORTDATE': str(cleaned_report_date)}
+        return z,d
+
+class LeavingForm(forms.Form):
+    report_date = forms.DateField(required=True, label='Дата отъезда')
+
+class LeavingReport():
+
+    def process(self, form):
+        cleaned_report_date = form.cleaned_data['report_date']
+        orders = Order.objects.filter(end_date = cleaned_report_date)
+        d = []
+        for order in orders:
+            d.append({'FIO':order.patient.fio(), 'ROOM':order.room.name})
+        z = {'REPORTDATE': str(cleaned_report_date)}
+        return z,d
+
+report_map = {"1":('Список заезжающих',EnteringForm,'simplereport.xls',EnteringReport()),
+              "2":('Список съезжающих',LeavingForm,'simplereport.xls',LeavingReport())}
+
+@login_required
+def processreport(request, tp):
+    report_metadata = report_map[tp]
+    form = report_metadata[1](request.POST)
+    if form.is_valid():
+        md = report_metadata[3].process(form)
+        tel = {'TITLE': report_metadata[0],
+               'P': md[1]}
+        for (key,value) in md[0].items():
+            tel[key] = str(value)
+
+        return fill_excel_template(report_metadata[2], tel)
+    else:
+        values = {"tp": tp, "metadata": report_metadata}
+        return render_to_response('pansionat/report.html', MenuRequestContext(request, values))
+
+@login_required
+def report(request, tp):
+    report_metadata = report_map[tp]
+    values = {"tp": tp, "metadata": report_metadata}
+    return render_to_response('pansionat/report.html', MenuRequestContext(request, values))
+
 class PatientForm(ModelForm):
     class Meta:
         model = Patient
